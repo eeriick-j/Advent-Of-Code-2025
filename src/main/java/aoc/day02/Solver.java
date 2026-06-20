965454eb2070370c5d6536fa7824b0436ffc7092
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-20 22:53:00 +0100

    Finished solve for part 1

--- src/main/java/aoc/day02/Solver.java
+++ src/main/java/aoc/day02/Solver.java
@@ -7,6 +7,26 @@ import java.util.List;
 public class Solver {
     public static void main(String[] args) {
         List<IDRange> idRanges = InputParser.parse(RawInputReader.read("inputs/day02.txt"));
-        for (IDRange idRange : idRanges) System.out.println(idRange);
+        System.out.println("Part 1: " + solvePart1(idRanges));
+    }
+
+    public static long solvePart1(List<IDRange> idRanges) {
+        ///  Suma de IDs inválidos (los que tengan dos mitades idénticas)
+        long sumInvalidIDS = 0;
+
+        for (IDRange range: idRanges) {
+            for(long id=range.lowerBound(); id<=range.upperBound(); id++){
+                // 1) Si es impar, no puede dividirse en dos mitades -> ID válido
+                String idStr = Long.toString(id);
+                if(idStr.length() % 2 != 0) continue;
+
+                // 2) ¿primera mitad == segunda mitad?
+                int halfIndex = idStr.length() / 2;
+                if(idStr.substring(0, halfIndex).equals(idStr.substring(halfIndex))){
+                    sumInvalidIDS += id;
+                }
+            }
+        }
+        return sumInvalidIDS;
     }
 }
