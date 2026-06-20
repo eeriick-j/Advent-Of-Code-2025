b9a76b5cf9ba8651220868b606e33ae8002b1a10
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-20 21:26:46 +0100

    First part done

--- src/main/java/aoc/day01/Solver.java
+++ src/main/java/aoc/day01/Solver.java
@@ -7,8 +7,21 @@ import java.util.List;
 public class Solver {
     public static void main(String[] args) {
         List<Rotation> rotations = InputParser.parse(RawInputReader.read("day01/a.txt"));
-        for (Rotation rotation : rotations) {
-            System.out.println(rotation);
-        }
+        System.out.println(solvePart1(rotations));
     }
+
+    public static int solvePart1(List<Rotation> rotations) {
+        int dialPointer = 50;
+        int numPointsTozero = 0;
+
+        for (Rotation rotation : rotations) {
+            if (rotation.direction() == 'L') dialPointer = (dialPointer - rotation.times() + 100) % 100;
+            else dialPointer = (dialPointer + rotation.times()) % 100;
+            if(dialPointer == 0) numPointsTozero++;
+        }
+
+        return numPointsTozero;
+    }
+
+    public static int solvePart2(List<Rotation> rotations) {return 0;}
 }
