8428066ec9baadd7f2b2d5070c46f95728b104ac
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-21 01:16:42 +0100

    Added size method to bank model

--- src/main/java/aoc/day03/Bank.java
+++ src/main/java/aoc/day03/Bank.java
@@ -2,4 +2,8 @@ package aoc.day03;
 
 import java.util.List;
 
-public record Bank(List<Integer> voltages) {}
+public record Bank(List<Integer> voltages) {
+    public int size(){
+        return voltages.size();
+    }
+}
--- src/main/java/aoc/day03/Solver.java
+++ src/main/java/aoc/day03/Solver.java
@@ -2,10 +2,27 @@ package aoc.day03;
 
 import tasks.RawInputReader;
 
+import java.util.List;
+
 public class Solver {
     public static void main(String[] args) {
-        for(Bank bank: InputParser.parse(RawInputReader.read("inputs/day03.txt"))){
-            System.out.println(bank);
+        List<Bank> banks = InputParser.parse(RawInputReader.read("inputs/day03.txt"));
+        System.out.println("Part 1: " + solvePart1(banks));
+    }
+
+    public static long solvePart1(List<Bank> banks) {
+        /// Sumas de las mayores parejas de 2 dígitos (concatenados() para cada banco
+        long sumVoltages = 0;
+        for (Bank bank : banks) {
+            int bestPair = 0;
+            for(int i=0; i<bank.size(); i++) {
+                for(int j=i+1; j<bank.size(); j++) {
+                    int currentPair = bank.voltages().get(i) * 10 +  bank.voltages().get(j);
+                    bestPair = Math.max(bestPair, currentPair);
+                }
+            }
+            sumVoltages += bestPair;
         }
+        return sumVoltages;
     }
 }
