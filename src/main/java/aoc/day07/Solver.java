d3b77a3c1ef134e2d8e971e9421c2e4d100f3040
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-21 17:19:46 +0100

    Added solution for part 2

--- src/main/java/aoc/day07/Solver.java
+++ src/main/java/aoc/day07/Solver.java
@@ -9,9 +9,11 @@ public class Solver {
     public static void main(String[] args) {
         char[][] grid = InputParser.parse(RawInputReader.read("inputs/day07.txt"));
         System.out.println("First part: " + solvePart1(grid));
+        System.out.println("Second part: " + solvePart2(grid));
     }
 
     public static int solvePart1(char[][] grid) {
+        ///  Número de veces que un rayo inicial se divide contra obstáculos
         final int start = new String(grid[0]).indexOf('S');
         Set<Integer> current = new HashSet<>();
         current.add(start);
@@ -32,5 +34,37 @@ public class Solver {
             current = next;
         }
         return splits;
+    }
+
+    public static long solvePart2(char[][] grid) {
+        ///  Número de paths distintos posibles (árbol)
+        final int start = new String(grid[0]).indexOf('S');
+
+        // Hay 1 forma de estar en la posición inicial, las demás posiciones tiene 0 formas
+        long[] current = new long[grid[0].length];
+        current[start] = 1;
+
+        for (int row = 1; row < grid.length; row++) {
+            // Caminos de la siguiente fila
+            long[] next = new long[grid[row].length];
+
+            for (int col = 0; col < current.length; col++) {
+                long ways = current[col];
+                if (ways == 0) continue;
+
+                if (grid[row][col] == '^') {
+                    if (col - 1 >= 0)           next[col - 1] += ways;
+                    if (col + 1 < next.length)  next[col + 1] += ways;
+                } else {
+                    next[col] += ways;
+                }
+            }
+            current = next;
+        }
+
+        // Suma de formas de llegar a cada fila v
+        long result = 0;
+        for (long v : current) result += v;
+        return result;
     }
 }
