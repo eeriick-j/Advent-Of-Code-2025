2fbf5dc3c5b60f916a966861727c74eb816e779a
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-22 00:16:44 +0100

    Both solvers done

--- src/main/java/aoc/day06/InputParser.java
+++ src/main/java/aoc/day06/InputParser.java
@@ -1,24 +1,9 @@
 package aoc.day06;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class InputParser {
-    public static Pair parse(String rawInput) {
-        List<String> lines = rawInput.lines().toList();
-
-        List<List<Integer>> values = lines.stream()
-                .filter(line -> line.matches(".*\\d.*"))
-                .map(line ->
-                        Arrays.stream(line.trim().split("\\s+"))
-                        .map(Integer::parseInt)
-                        .toList())
-                .toList();
-
-        List<Character> operators = Arrays.stream(lines.getLast().trim().split("\\s+"))
-                .map(s -> s.charAt(0))
-                .toList();
-
-        return new Pair(values, operators);
+    public static List<String> parse(String rawInput) {
+        return rawInput.lines().toList();
     }
 }
--- src/main/java/aoc/day06/Pair.java
+++
@@ -1,5 +0,0 @@
-package aoc.day06;
-
-import java.util.List;
-
-public record Pair(List<List<Integer>> values, List<Character> operators) {}
