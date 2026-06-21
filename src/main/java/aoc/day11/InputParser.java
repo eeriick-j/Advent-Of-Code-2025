fc0266cec71fe6104d7b22a89d86c4263c455790
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-21 20:59:54 +0100

    Added Graph model and input parser

---
+++ src/main/java/aoc/day11/InputParser.java
@@ -0,0 +1,23 @@
+package aoc.day11;
+
+import java.util.*;
+
+public class InputParser {
+
+    public static Graph parse(String rawInput) {
+        Graph graph = new Graph();
+        String[] lines = rawInput.split("\\n");
+
+        for (String line : lines) {
+            line = line.trim();
+            if (line.isEmpty()) continue;
+
+            String[] parts = line.split(":");
+            String from = parts[0].trim();
+            String[] tos = parts[1].trim().split("\\s+");
+
+            for (String to : tos) graph.addEdge(from, to);
+        }
+        return graph;
+    }
+}
