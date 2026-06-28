2b08eff900c8232d85da103ea2ac9714158d287b
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-28 16:20:45 +0100

    Added several tests for Day05 and Day06

--- src/test/java/days/day05/Day05Test.java
+++ src/test/java/days/day05/Day05Test.java
@@ -2,9 +2,14 @@ package days.day05;
 
 import aoc.core.DaySolver;
 import aoc.days.day05.Day05;
+import aoc.days.day05.Day05Parser;
+import aoc.days.day05.model.IDRange;
+import aoc.days.day05.model.Pair;
 import aoc.io.FileReader;
 import aoc.io.TXTFileReader;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,5 +31,124 @@ public class Day05Test {
     public void solvePart2Correct() {
         DaySolver solver = Day05.build(input());
         assertEquals(344771884978261L, solver.solvePart2());
+    }
+
+    @Test
+    public void parseSeparatesRangesAndIds() {
+        String input = """
+                10-20
+                35
+                40-50
+                7
+                """;
+
+        Pair pair = new Day05Parser().parse(input);
+
+        assertEquals(
+                List.of(
+                        new IDRange(10,20),
+                        new IDRange(40,50)
+                ),
+                pair.idRanges()
+        );
+        assertEquals(List.of(35L,7L), pair.ids());
+    }
+
+    @Test
+    public void parseIgnoresEmptyLines() {
+        String input = """
+
+                1-3
+
+                5
+
+                """;
+
+        Pair pair = new Day05Parser().parse(input);
+        assertEquals(1, pair.idRanges().size());
+        assertEquals(1, pair.ids().size());
+    }
+
+    @Test
+    public void countsIdsInsideRanges() {
+        String input = """
+                10-20
+                30-40
+                5
+                10
+                15
+                20
+                21
+                35
+                50
+                """;
+
+        assertEquals(4L, Day05.build(input).solvePart1());
+    }
+
+    @Test
+    public void boundariesBelongToRange() {
+        String input = """
+                100-200
+                100
+                200
+                99
+                201
+                """;
+
+        assertEquals(2L, Day05.build(input).solvePart1());
+    }
+
+    @Test
+    public void countsIndependentRanges() {
+        String input = """
+            1-3
+            10-12
+            """;
+
+        assertEquals(6L, Day05.build(input).solvePart2());
+    }
+
+    @Test
+    public void mergesOverlappingRanges() {
+        String input = """
+            10-15
+            13-20
+            """;
+
+        assertEquals(11L, Day05.build(input).solvePart2());
+    }
+
+    @Test
+    public void mergesAdjacentRanges() {
+        String input = """
+            1-5
+            6-10
+            """;
+
+        assertEquals(10L, Day05.build(input).solvePart2());
+    }
+
+    @Test
+    public void mergesSeveralRangesIntoOne() {
+        String input = """
+            1-3
+            2-5
+            6-8
+            8-10
+            """;
+
+        assertEquals(10L, Day05.build(input).solvePart2());
+    }
+
+    @Test
+    public void sortsRangesBeforeMerging() {
+        String input = """
+            20-30
+            1-5
+            4-10
+            """;
+
+        assertEquals(21L, Day05.build(input).solvePart2());
     }
 }
--- src/test/java/days/day06/Day06Test.java
+++ src/test/java/days/day06/Day06Test.java
@@ -2,9 +2,12 @@ package days.day06;
 
 import aoc.core.DaySolver;
 import aoc.days.day06.Day06;
+import aoc.days.day06.Day06Parser;
 import aoc.io.FileReader;
 import aoc.io.TXTFileReader;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,5 +29,89 @@ public class Day06Test {
     public void solvePart2Correct() {
         DaySolver solver = Day06.build(input());
         assertEquals(8674740488592L, solver.solvePart2());
+    }
+
+    @Test
+    public void parseSplitsLines() {
+        Day06Parser parser = new Day06Parser();
+        List<String> result = parser.parse("""
+                abc
+                def
+                ghi
+                """);
+
+        assertEquals(List.of("abc", "def", "ghi"), result);
+    }
+
+    @Test
+    public void solvePart1AddsIndependentBlocks() {
+        String input = """
+            1 2
+            3 4
+            + +
+            """;
+
+        Day06 solver = (Day06) Day06.build(input);
+        assertEquals(Long.valueOf(10), solver.solvePart1());
+    }
+
+    @Test
+    public void solvePart1MultipliesRows() {
+        String input = """
+            2
+            3
+            *
+            """;
+
+        Day06 solver = (Day06) Day06.build(input);
+        assertEquals(Long.valueOf(6), solver.solvePart1());
+    }
+
+    @Test
+    public void solvePart1ReadsMultiDigitNumbers() {
+        String input = """
+            12
+            34
+            ++
+            """;
+
+        Day06 solver = (Day06) Day06.build(input);
+        assertEquals(Long.valueOf(46), solver.solvePart1());
+    }
+
+    @Test
+    public void solvePart2ReadsColumnsRightToLeft() {
+        String input = """
+            12
+            34
+            ++
+            """;
+
+        Day06 solver = (Day06) Day06.build(input);
+        assertEquals(Long.valueOf(37), solver.solvePart2());
+    }
+
+    @Test
+    public void solvePart1IgnoresBlockWithoutOperator() {
+        String input = """
+            12
+            34
+
+            """;
+
+        Day06 solver = (Day06) Day06.build(input);
+        assertEquals(Long.valueOf(0), solver.solvePart1());
+    }
+
+    @Test
+    public void solvePart1SeparatesBlocksUsingEmptyColumns() {
+        String input = """
+            1 5
+            2 6
+            + +
+            """;
+
+        Day06 solver = (Day06) Day06.build(input);
+        assertEquals(Long.valueOf(14), solver.solvePart1());
     }
 }
