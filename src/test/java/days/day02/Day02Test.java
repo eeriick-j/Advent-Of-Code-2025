4ee9106a0e55543f8e69409cd15e48e5e22521ff
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-28 15:59:57 +0100

    Added several tests for Day02

--- src/test/java/days/day02/Day02Test.java
+++ src/test/java/days/day02/Day02Test.java
@@ -2,9 +2,13 @@ package days.day02;
 
 import aoc.core.DaySolver;
 import aoc.days.day02.Day02;
+import aoc.days.day02.Day02Parser;
+import aoc.days.day02.model.IDRange;
 import aoc.io.FileReader;
 import aoc.io.TXTFileReader;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,5 +30,72 @@ public class Day02Test {
     public void solvePart2Correct() {
         DaySolver solver = Day02.build(input());
         assertEquals(45814076230L, solver.solvePart2());
+    }
+
+    @Test
+    public void parsesSingleRange() {
+        List<IDRange> ranges = new Day02Parser().parse("10-20");
+
+        assertEquals(1, ranges.size());
+        assertEquals(new IDRange(10, 20), ranges.getFirst());
+    }
+
+    @Test
+    public void parsesMultipleRanges() {
+        List<IDRange> ranges = new Day02Parser().parse("1-2,5-8,20-30");
+
+        assertEquals(List.of(
+                new IDRange(1, 2),
+                new IDRange(5, 8),
+                new IDRange(20, 30)
+        ), ranges);
+    }
+
+    @Test
+    public void oddLengthIdsAreValid() {
+        Day02 solver = new Day02(List.of(new IDRange(111, 111)));
+        assertEquals(Long.valueOf(0), solver.solvePart1());
+    }
+
+    @Test
+    public void identicalHalvesAreInvalid() {
+        Day02 solver = new Day02(List.of(new IDRange(1212, 1212)));
+        assertEquals(Long.valueOf(1212), solver.solvePart1());
+    }
+
+    @Test
+    public void differentHalvesAreValid() {
+        Day02 solver = new Day02(List.of(new IDRange(1234, 1234)));
+        assertEquals(Long.valueOf(0), solver.solvePart1());
+    }
+
+    @Test
+    public void sumsOnlyInvalidIds() {
+        Day02 solver = new Day02(List.of(new IDRange(1212, 1214)));
+        assertEquals(Long.valueOf(1212), solver.solvePart1());
+    }
+
+    @Test
+    public void repeatedPatternIsInvalid() {
+        Day02 solver = new Day02(List.of(new IDRange(1212, 1212)));
+        assertEquals(Long.valueOf(1212), solver.solvePart2());
+    }
+
+    @Test
+    public void repeatedSingleDigitPatternIsInvalid() {
+        Day02 solver = new Day02(List.of(new IDRange(1111, 1111)));
+        assertEquals(Long.valueOf(1111), solver.solvePart2());
+    }
+
+    @Test
+    public void nonRepeatedPatternIsValid() {
+        Day02 solver = new Day02(List.of(new IDRange(1234, 1234)));
+        assertEquals(Long.valueOf(0), solver.solvePart2());
+    }
+
+    @Test
+    public void sumsAllInvalidIds() {
+        Day02 solver = new Day02(List.of(new IDRange(1212, 1213)));
+        assertEquals(Long.valueOf(1212), solver.solvePart2());
     }
 }
