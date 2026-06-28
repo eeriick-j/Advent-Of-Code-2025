c6c131911d78e89f99eaead8505bbc9ff77fa7b2
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-28 16:10:15 +0100

    Added several tests for Day04

--- src/test/java/days/day04/Day04Test.java
+++ src/test/java/days/day04/Day04Test.java
@@ -2,10 +2,12 @@ package days.day04;
 
 import aoc.core.DaySolver;
 import aoc.days.day04.Day04;
+import aoc.days.day04.Day04Parser;
 import aoc.io.FileReader;
 import aoc.io.TXTFileReader;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class Day04Test {
@@ -27,4 +29,99 @@ public class Day04Test {
         DaySolver solver = Day04.build(input());
         assertEquals(9280, solver.solvePart2());
     }
+
+    @Test
+    public void parsesMatrixCorrectly() {
+        String input =
+                "@.\n" +
+                        ".@";
+
+        char[][] result = new Day04Parser().parse(input);
+
+        assertEquals(2, result.length);
+        assertArrayEquals(new char[]{'@','.'}, result[0]);
+        assertArrayEquals(new char[]{'.','@'}, result[1]);
+    }
+
+    @Test
+    public void singleRollIsAccessible() {
+        DaySolver solver = Day04.build("@");
+        assertEquals(1, solver.solvePart1());
+    }
+
+    @Test
+    public void singleRollRemovedInPart2() {
+        DaySolver solver = Day04.build("@");
+        assertEquals(1, solver.solvePart2());
+    }
+
+    @Test
+    public void isolatedRollsAreAccessible() {
+        String input = "@.@\n" +
+                        "...\n" +
+                        "@.@";
+
+        DaySolver solver = Day04.build(input);
+        assertEquals(4, solver.solvePart1());
+    }
+
+    @Test
+    public void block2x2AllAccessible() {
+        String input = "@@\n" +
+                       "@@";
+
+        DaySolver solver = Day04.build(input);
+        assertEquals(4, solver.solvePart1());
+    }
+
+    @Test
+    public void full3x3OnlyCornersAccessible() {
+        String input = "@@@\n" +
+                       "@@@\n" +
+                       "@@@";
+
+        DaySolver solver = Day04.build(input);
+        assertEquals(4, solver.solvePart1());
+    }
+
+    @Test
+    public void part2RemovesRollsInSeveralRounds() {
+        String input = "@@@\n" +
+                       "@@@\n" +
+                       "@@@";
+
+        DaySolver solver = Day04.build(input);
+        assertEquals(9, solver.solvePart2());
+    }
+
+    @Test
+    public void emptyWarehouse() {
+        String input = "...\n" +
+                       "...";
+
+        DaySolver solver = Day04.build(input);
+        assertEquals(0, solver.solvePart1());
+        assertEquals(0, solver.solvePart2());
+    }
+
+    @Test
+    public void cornerRollHasNoNeighbours() {
+        DaySolver solver = Day04.build(
+                  "@.\n" +
+                        ".."
+        );
+        assertEquals(1, solver.solvePart1());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
