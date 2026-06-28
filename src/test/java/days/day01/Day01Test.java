136d78b26fc79a5d82ce691a5e4097817bdd72fc
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-06-28 15:51:39 +0100

    Added several tests for Day01

--- src/test/java/days/day01/Day01Test.java
+++ src/test/java/days/day01/Day01Test.java
@@ -2,9 +2,14 @@ package days.day01;
 
 import aoc.core.DaySolver;
 import aoc.days.day01.Day01;
+import aoc.days.day01.Day01Parser;
+import aoc.days.day01.Dial;
+import aoc.days.day01.model.Rotation;
 import aoc.io.FileReader;
 import aoc.io.TXTFileReader;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,5 +31,93 @@ public class Day01Test {
     public void solvePart2Correct() {
         DaySolver solver = Day01.build(input());
         assertEquals(6695, solver.solvePart2());
+    }
+
+    @Test
+    public void parseSeveralRotations() {
+        String input = """
+                L10
+                R3
+                L99
+                """;
+
+        List<Rotation> rotations = new Day01Parser().parse(input);
+
+        assertEquals(3, rotations.size());
+        assertEquals(new Rotation('L', 10), rotations.get(0));
+        assertEquals(new Rotation('R', 3), rotations.get(1));
+        assertEquals(new Rotation('L', 99), rotations.get(2));
+    }
+
+    @Test
+    public void rotateFullDoesNotCountWhenNotEndingAtZero() {
+        Dial dial = new Dial();
+        dial.rotateFull(new Rotation('R', 10));
+
+        assertEquals(0, dial.numPointsToZero());
+    }
+
+    @Test
+    public void rotateFullCountsWhenEndingAtZero() {
+        Dial dial = new Dial();
+        dial.rotateFull(new Rotation('R', 50));
+
+        assertEquals(1, dial.numPointsToZero());
+    }
+
+    @Test
+    public void rotateFullCountsSeveralTimes() {
+        Dial dial = new Dial();
+        dial.rotateFull(new Rotation('R', 50));
+        dial.rotateFull(new Rotation('R', 100));
+        dial.rotateFull(new Rotation('L', 100));
+
+        assertEquals(3, dial.numPointsToZero());
+    }
+
+    @Test
+    public void rotateClickByClickCountsCrossingZero() {
+        Dial dial = new Dial();
+        dial.rotateClickByClick(new Rotation('R', 50));
+
+        assertEquals(1, dial.numClicksToZero());
+    }
+
+    @Test
+    public void rotateClickByClickCountsMultipleCrossings() {
+        Dial dial = new Dial();
+        dial.rotateClickByClick(new Rotation('R', 150));
+
+        assertEquals(2, dial.numClicksToZero());
+    }
+
+    @Test
+    public void rotateClickByClickCountsCrossingZeroLeft() {
+        Dial dial = new Dial();
+        dial.rotateClickByClick(new Rotation('L', 50));
+
+        assertEquals(1, dial.numClicksToZero());
+    }
+
+    @Test
+    public void solvePart1SmallExample() {
+        String input = """
+            R50
+            R50
+            L100
+            """;
+
+        DaySolver day = Day01.build(input);
+        assertEquals(1, day.solvePart1());
+    }
+
+    @Test
+    public void solvePart2SmallExample() {
+        String input = """
+            R150
+            """;
+
+        DaySolver day = Day01.build(input);
+        assertEquals(2, day.solvePart2());
     }
 }
